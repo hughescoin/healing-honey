2cbd75fafd22118a8e1024d6484393d800e17132
Patrick Hughes <patrick.hughes@coinbase.com>
2024-12-07 08:50:55 -0600

    remove logging

--- src/app/api/createcharge/route.ts
+++ src/app/api/createcharge/route.ts
@@ -4,11 +4,6 @@ import { COMMERCE_API_URL } from 'src/links';
 export async function POST(request: NextRequest) {
   try {
     const chargeDetails = await request.json();
-    console.log('API Route - Received charge details:', chargeDetails);
-    console.log(
-      'API Route - Using API key:',
-      process.env.COINBASE_COMMERCE_API_KEY
-    );
 
     const res = await fetch(`${COMMERCE_API_URL}/charges`, {
       method: 'POST',
@@ -20,10 +15,8 @@ export async function POST(request: NextRequest) {
     });
 
     const data = await res.json();
-    console.log('API Route - Coinbase response:', data);
     return NextResponse.json(data);
   } catch (error) {
-    console.error('API Route - Error:', error);
     return NextResponse.json(
       { error: 'Failed to create charge' },
       { status: 500 }
